97572b9d1f67cf462facd94a1cfa20ef2c3f1766
sdyalor <sdyalor@localhost.localdomain>
2020-03-11 07:44:13 -0500

    angular with eslint

---
+++ scripts.sh
@@ -0,0 +1,26 @@
+mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.4 -Dpackaging=jar
+
+/home/julio_torres_c/IBM/WebSphere/AppServer/plugins/com.ibm.ws.runtime.jar
+
+mvn install:install-file -Dfile=com.ibm.ws.runtime.jar -DgroupId=com.ibm -DartifactId=ws.runtime -Dversion=9.0.5 -Dpackaging=jar
+
+
+yarn add -D @typescript-eslint/eslint-plugin @typescript-eslint/parser
+
+npm i eslint-plugin-jsdoc
+npm install eslint-plugin-import --save-dev
+  "prefer-arrow"
+
+yarn add -D eslint-plugin-import @typescript-eslint/parser eslint-import-resolver-typescript
+
+
+
+
+https://www.npmjs.com/package/eslint-import-resolver-typescript
+https://www.npmjs.com/package/eslint-plugin-prefer-arrow
+https://www.npmjs.com/package/eslint-plugin-jsdoc
+https://preview.npmjs.com/package/eslint-plugin-import
+https://www.npmjs.com/package/@angular-eslint/eslint-plugin
+
+https://github.com/typescript-eslint/typescript-eslint#package-versions
+https://github.com/mgechev/codelyzer/issues/763
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'simulador'`, () => {
+  it('should have as title \'simulador\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('simulador');
